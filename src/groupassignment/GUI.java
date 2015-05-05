d84fbedcb903ca8119a6c28336ca7cdda5e180ef
htokuda10 <htokuda10@gmail.com>
2015-05-05 11:47:38 -0600

    Commenting Completed

--- src/groupassignment/GUI.java
+++ src/groupassignment/GUI.java
@@ -12,7 +12,6 @@ import java.awt.event.AdjustmentListener;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-
 /**
  * The GUI class creates a window that allows a user to run a simulation with
  * the option to adjust 4 different parameters.
@@ -20,7 +19,7 @@ import java.io.IOException;
  */
 public class GUI {
 
-    // frames, panes, and automobiles...or...panels
+    // Frames, panes, and automobiles...or...panels.
     private JFrame frame;
     private JLabel contentPane;
     private JPanel northPanel;
@@ -30,40 +29,40 @@ public class GUI {
     private JPanel centerPanel;
     private JPanel eastPanel;
 
-    // used in setting/displaying simulation time value
+    // Used in setting/displaying simulation time value.
     private JSlider totalTimeSlider;
     private JLabel totalTimeLabel;
     private JLabel totalTimeValue;
 
-    // used in setting/displaying question mean time
+    // Used in setting/displaying question mean time.
     private JSlider questionMeanSlider;
     private JLabel questionMeanLabel;
     private JLabel questionMeanValue;
     private int questionSim;
 
-    // used in setting/displaying walk-in mean time
+    // Used in setting/displaying walk-in mean time.
     private JSlider walkInMeanSlider;
     private JLabel walkInMeanLabel;
     private JLabel walkInMeanValue;
     private int walkInSim;
 
-    // used in setting/displaying call-in mean time
+    // Used in setting/displaying call-in mean time.
     private JSlider callInMeanSlider;
     private JLabel callInMeanLabel;
     private JLabel callInMeanValue;
     private int callInSim;
 
-    // buttons
+    // Buttons.
     private JButton simulate;
     private JButton save;
     private JButton reset;
     private JButton exit;
 
-    // style settings
+    // Style settings.
     private Color transparent = new Color(0, 0, 0, 0);
     private Font large = new Font("Sans Serif", Font.PLAIN, 18);
 
-    // custom background images for the JPanels
+    // Custom background images for the JPanels.
     private ImageIcon bgImagePath = new ImageIcon(getClass().getClassLoader()
             .getResource("groupassignment/resources/dash.jpg"));
     private ImageIcon bgWest = new ImageIcon(getClass().getClassLoader()
@@ -73,7 +72,7 @@ public class GUI {
     private ImageIcon bgEast = new ImageIcon(getClass().getClassLoader()
             .getResource("groupassignment/resources/dash_east.jpg"));
 
-    // custom images for the buttons
+    // Custom images for the buttons.
     ImageIcon simulateIcon = new ImageIcon(getClass().getClassLoader()
             .getResource("groupassignment/resources/btn_simulate.png"));
     ImageIcon saveIcon = new ImageIcon(getClass().getClassLoader()
@@ -85,30 +84,30 @@ public class GUI {
 
     // -------------------------------------------------------------------------
     /**
-     * GUI constructor
+     * GUI constructor.
      */
     public GUI () {
 
-        // use JLabel as content pane (image background)
+        // Use JLabel as content pane (image background).
         contentPane = new JLabel(bgImagePath);
         contentPane.setLayout(new BorderLayout());
 
-        // new frame filled with the JLabel
+        // New frame filled with the JLabel.
         frame = new JFrame("The Secretary");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setResizable(false);
         frame.setLayout(new BorderLayout());
         frame.add(contentPane); // add JLabel
 
-        // North --------------------------------
+        //==================== North Panel ===============================
 
-        // this panel displays the settings before running a simulation
+        // This panel displays the settings before running a simulation.
         northPanel = new JPanel(new GridLayout(4, 2));
         northPanel.setBorder(new EmptyBorder(10, 20, 20, 0));
         northPanel.setPreferredSize(new Dimension(900, 342));
         northPanel.setBackground(transparent);
 
-        // display value from totalTimeSlider
+        // Display value from totalTimeSlider.
         totalTimeLabel = new JLabel("Total simulation time: ");
         totalTimeLabel.setHorizontalAlignment(SwingConstants.RIGHT);
         totalTimeLabel.setFont(large);
@@ -119,7 +118,7 @@ public class GUI {
         northPanel.add(totalTimeLabel);
         northPanel.add(totalTimeValue);
 
-        // display value from questionMeanSlider
+        // Display value from questionMeanSlider.
         questionMeanLabel = new JLabel("Average time to answer a question: ");
         questionMeanLabel.setHorizontalAlignment(SwingConstants.RIGHT);
         questionMeanLabel.setFont(large);
@@ -130,7 +129,7 @@ public class GUI {
         northPanel.add(questionMeanLabel);
         northPanel.add(questionMeanValue);
 
-        // display value from walkInMeanSlider
+        // Display value from walkInMeanSlider.
         walkInMeanLabel = new JLabel("Average time between walk-ins: ");
         walkInMeanLabel.setHorizontalAlignment(SwingConstants.RIGHT);
         walkInMeanLabel.setFont(large);
@@ -141,7 +140,7 @@ public class GUI {
         northPanel.add(walkInMeanLabel);
         northPanel.add(walkInMeanValue);
 
-        // display value from callInMeanSlider
+        // Display value from callInMeanSlider.
         callInMeanLabel = new JLabel("Average time between phone calls: ");
         callInMeanLabel.setHorizontalAlignment(SwingConstants.RIGHT);
         callInMeanLabel.setFont(large);
@@ -152,24 +151,24 @@ public class GUI {
         northPanel.add(callInMeanLabel);
         northPanel.add(callInMeanValue);
 
-        // add north panel to the content pane
+        // Add north panel to the content pane.
         contentPane.add(northPanel, BorderLayout.NORTH);
 
-        // West ---------------------------------
+        //==================== West Panel ================================
 
-        // this panel only displays a section of background image
+        // This panel only displays a section of background image
         westPanel = new JPanel();
         westPanel.setPreferredSize(new Dimension(335, 359));
         westPanel.setLayout(new BorderLayout());
         JLabel westBG = new JLabel(bgWest);
         westPanel.add(westBG);
 
-        // add west panel to the content pane
+        // Add west panel to the content pane
         contentPane.add(westPanel, BorderLayout.WEST);
 
-        // Center -------------------------------
+        //==================== Center Panel ==============================
 
-        // this panel contains the 4 sliders for simulation settings
+        // This panel contains the 4 sliders for simulation settings.
         centerPanel = new JPanel();
         centerPanel.setLayout(new BorderLayout());
         JLabel centerBG = new JLabel(bgCenter);
@@ -177,7 +176,7 @@ public class GUI {
         centerBG.setLayout(new BoxLayout(centerBG, BoxLayout.Y_AXIS));
         centerBG.add(Box.createVerticalStrut(70));
 
-        // slider for total time
+        // Slider for total time.
         totalTimeSlider = new JSlider(0, 0, 100, 50);
         totalTimeSlider.setMaximumSize(new Dimension(320, 50));
         totalTimeSlider.setPaintLabels(true);
@@ -186,7 +185,7 @@ public class GUI {
         totalTimeSlider.setOpaque(false);
         totalTimeSlider.addChangeListener(new ChangeListener() {
             @Override
-            // display the changing value if/as the slider moves
+            // Display the changing value if/as the slider moves.
             public void stateChanged(ChangeEvent e) {
                 totalTimeValue.setText(totalTimeSlider.getValue() + " minutes");
                 if (totalTimeSlider.getValue() == 0)
@@ -197,7 +196,7 @@ public class GUI {
         centerBG.add(totalTimeSlider);
         totalTimeValue.setText(totalTimeSlider.getValue() + " minutes");
 
-        // slider for average question time
+        // Slider for average question time.
         questionMeanSlider = new JSlider(0, 0, 100, 24);
         questionMeanSlider.setMaximumSize(new Dimension(320, 50));
         questionMeanSlider.setPaintLabels(true);
@@ -206,18 +205,21 @@ public class GUI {
         questionMeanSlider.setOpaque(false);
         questionMeanSlider.addChangeListener(new ChangeListener() {
             @Override
-            // display the changing value if/as the slider moves
+            // Display the changing value if/as the slider moves.
             public void stateChanged(ChangeEvent e) {
-                questionMeanValue.setText(questionMeanSlider.getValue() + " seconds");
+                questionMeanValue.setText(questionMeanSlider.getValue()
+                        + " seconds");
+                
                 if (questionMeanSlider.getValue() == 0)
                     questionMeanValue.setText("Infinity");
                 frame.repaint();
             }
         });
         centerBG.add(questionMeanSlider);
-        questionMeanValue.setText(questionMeanSlider.getValue() + " seconds");
+        questionMeanValue.setText(questionMeanSlider.getValue()
+                + " seconds");
 
-        // slider for average time between walk-ins
+        // Slider for average time between walk-ins.
         walkInMeanSlider = new JSlider(0, 0, 100, 45);
         walkInMeanSlider.setMaximumSize(new Dimension(320, 50));
         walkInMeanSlider.setPaintLabels(true);
@@ -226,9 +228,11 @@ public class GUI {
         walkInMeanSlider.setOpaque(false);
         walkInMeanSlider.addChangeListener(new ChangeListener() {
             @Override
-            // display the changing value if/as the slider moves
+            // Display the changing value if/as the slider moves.
             public void stateChanged(ChangeEvent e) {
-                walkInMeanValue.setText(walkInMeanSlider.getValue() + " seconds");
+                walkInMeanValue.setText(walkInMeanSlider.getValue()
+                        + " seconds");
+                
                 if (walkInMeanSlider.getValue() == 0)
                     walkInMeanValue.setText("Infinity");
                 frame.repaint();
@@ -237,7 +241,7 @@ public class GUI {
         centerBG.add(walkInMeanSlider);
         walkInMeanValue.setText(walkInMeanSlider.getValue() + " seconds");
 
-        // slider for average time between phone calls
+        // Slider for average time between phone calls.
         callInMeanSlider = new JSlider(0, 0, 100, 55);
         callInMeanSlider.setMaximumSize(new Dimension(320, 50));
         callInMeanSlider.setPaintLabels(true);
@@ -246,9 +250,11 @@ public class GUI {
         callInMeanSlider.setOpaque(false);
         callInMeanSlider.addChangeListener(new ChangeListener() {
             @Override
-            // display the changing value if/as the slider moves
+            // Display the changing value if/as the slider moves.
             public void stateChanged(ChangeEvent e) {
-                callInMeanValue.setText(callInMeanSlider.getValue() + " seconds");
+                callInMeanValue.setText(callInMeanSlider.getValue()
+                        + " seconds");
+                
                 if (callInMeanSlider.getValue() == 0)
                     callInMeanValue.setText("Infinity");
                 frame.repaint();
@@ -257,12 +263,12 @@ public class GUI {
         centerBG.add(callInMeanSlider);
         callInMeanValue.setText(callInMeanSlider.getValue() + " seconds");
 
-        // add center panel to the content pane
+        // Add center panel to the content pane.
         contentPane.add(centerPanel, BorderLayout.CENTER);
 
-        // East ---------------------------------
+        //==================== East Panel ================================
 
-        // this panel contains the buttons
+        // This panel contains the buttons.
         eastPanel = new JPanel();
         eastPanel.setBackground(new Color(212, 214, 211));
         eastPanel.setLayout(new BorderLayout());
@@ -271,7 +277,7 @@ public class GUI {
         eastPanel.add(eastBG);
         eastBG.setLayout(new BoxLayout(eastBG, BoxLayout.Y_AXIS));
 
-        // [simulate] button
+        // [simulate] button.
         simulate = new JButton();
         simulate.setIcon(simulateIcon);
         simulate.setOpaque(false);
@@ -281,27 +287,27 @@ public class GUI {
         simulate.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // can't run a simulation for zero minutes
+                // Can't run a simulation for zero minutes.
                 if (totalTimeSlider.getValue() == 0) {
                     JOptionPane.showMessageDialog(frame,
                             "Simulate for ZERO minutes?\nOk, done! No results.",
                             "Really?", JOptionPane.PLAIN_MESSAGE);
                     return;
                 }
-                displayResults(); // run simulation and display results
+                displayResults(); // Run simulation and display results.
 
-                // no need for these to be enabled at this point
+                // No need for these to be enabled at this point.
                 simulate.setEnabled(false);
                 totalTimeSlider.setEnabled(false);
                 questionMeanSlider.setEnabled(false);
                 walkInMeanSlider.setEnabled(false);
                 callInMeanSlider.setEnabled(false);
-                save.setEnabled(true); // enable this one
+                save.setEnabled(true);
             }
         });
         eastBG.add(simulate);
 
-        // [save] button
+        // [save] button.
         save = new JButton();
         save.setEnabled(false);
         save.setIcon(saveIcon);
@@ -311,7 +317,7 @@ public class GUI {
         save.setFocusPainted(false);
         save.addActionListener(new ActionListener() {
             @Override
-            // allow user to save results as a text file
+            // Allow user to save results as a text file.
             public void actionPerformed(ActionEvent e) {
                 JFileChooser chooser = new JFileChooser();
                 int status = chooser.showSaveDialog(frame);
@@ -321,13 +327,15 @@ public class GUI {
                         FileWriter fw = new FileWriter(selectedFile + ".txt");
                         results.write(fw);
                     }
-                    catch (IOException ioe) { /* nothing */ }
+                    catch (IOException ioe) {
+                        // Do nothing.
+                    }
                 }
             }
         });
         eastBG.add(save);
 
-        // [reset] button
+        // [reset] button.
         reset = new JButton();
         reset.setIcon(resetIcon);
         reset.setOpaque(false);
@@ -336,7 +344,7 @@ public class GUI {
         reset.setFocusPainted(false);
         reset.addActionListener(new ActionListener() {
             @Override
-            // close current window and run program again
+            // Close current window and run program again.
             public void actionPerformed(ActionEvent e) {
                 frame.dispose();
                 String[] args = {};
@@ -345,7 +353,7 @@ public class GUI {
         });
         eastBG.add(reset);
 
-        // [exit] button
+        // [exit] button.
         exit = new JButton();
         exit.setIcon(exitIcon);
         exit.setOpaque(false);
@@ -360,10 +368,10 @@ public class GUI {
         });
         eastBG.add(exit);
 
-        // add east panel to the content pane
+        // Add east panel to the content pane.
         contentPane.add(eastPanel, BorderLayout.EAST);
 
-        // center the window and make visible
+        // Center the window and make visible.
         Point center = GraphicsEnvironment
                 .getLocalGraphicsEnvironment().getCenterPoint();
         frame.setBounds(center.x - 900 / 2, center.y - 720 / 2, 900, 720);
@@ -371,17 +379,15 @@ public class GUI {
 
     }  // GUI constructor
 
-    // -------------------------------------------------------------------------
-
     /**
      * Displays the results of the simulation in a scroll pane.
      */
     public void displayResults() {
 
-        // remove simulation options and display results instead
+        // Remove simulation options and display results instead.
         northPanel.removeAll();
         northPanel.setLayout(new BorderLayout());
-        // display the chosen settings for this simulation
+        // Display the chosen settings for this simulation.
         results = new JTextArea("Results from a(n) " +
                 totalTimeSlider.getValue() + "-minute simulation with the" +
                 " following settings: \n\n- " + questionMeanSlider.getValue() +
@@ -395,23 +401,24 @@ public class GUI {
                 "--------------------------------------------------------\n\n");
         results.setForeground(Color.white);
         results.setOpaque(false);
-        // will allow user to scroll through results
+        // Will allow user to scroll through results.
         resultsPane = new JScrollPane(results);
         resultsPane.setOpaque(false);
         resultsPane.getViewport().setOpaque(false);
         JScrollBar scrollBar = resultsPane.getVerticalScrollBar();
         scrollBar.addAdjustmentListener(new AdjustmentListener() {
             @Override
-            // repaint the background if the user scrolls the pane
+            // Repaint the background if the user scrolls the pane.
             public void adjustmentValueChanged(AdjustmentEvent e) {
                 frame.repaint();
             }
         });
-        results.setEditable(false); // make results read-only
-        northPanel.add(resultsPane); // add the scroll pane to the north
+         // Make results read-only.
+        results.setEditable(false);
+        northPanel.add(resultsPane);
         northPanel.validate();
 
-        // if any slider is zero (displayed as infinity), simulate as infinity
+        // If any slider is zero (displayed as infinity), simulate as infinity.
         if (questionMeanSlider.getValue() == 0) {
             questionSim = Integer.MAX_VALUE;
         } else {
@@ -428,11 +435,9 @@ public class GUI {
             callInSim = callInMeanSlider.getValue();
         }
 
-        // run the simulation with chosen settings
+        // Run the simulation with chosen settings.
         new Timer(totalTimeSlider.getValue() * 60, // convert to seconds
                 questionSim, walkInSim, callInSim, results);
         results.append("\n\nEnd of Simulation.");
-
     }  // displayResults
-
 }  // GUI class
--- src/groupassignment/GroupAssignment.java
+++ src/groupassignment/GroupAssignment.java
@@ -1,6 +1,22 @@
 package groupassignment;
 /**
- *
+ * GroupAssignment is a program that is designed to simulate an office business
+ * that has two types of customers, walk-ins and call-ins.  A GUI will appear
+ * upon startup which will allow the user to select four time values: simulation
+ * time, walk-in mean time, call-in mean time, and time it takes to answer a
+ * question or question mean time.  The program will then run the simulation,
+ * and using a linked list as the queue, it will place walk-in customers at the
+ * tail of the linked list and call-in customers will be placed at the head of
+ * the linked list.  Poisson's random formula is used to determine three values
+ * using the mean values provided by the user prior to starting the simulation.
+ * Once the simulation has started, it uses a loop to simulate the time, each
+ * iteration counts as one second.  When the loop reaches the walk-in/call-in
+ * value, a customer is created and placed into the queue based on the rules
+ * provided above, and new values are created relative to the customer created;
+ * a walk-in customer created equals a new Poissons's value assigned to the
+ * walk-in variable to determine when the next walk-in customer will be created,
+ * and repeat until the user defined simulation time has been reached.  The 
+ * program will print out the results after the simulation has completed.
  * @author Hideyuki Tokuda, Jason Mathews, Jedediah Hernandez
  */
 public class GroupAssignment {
